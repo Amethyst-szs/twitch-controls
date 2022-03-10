32cda9d1364ee91bef79183eec01d55fb2743c8b
Amethyst-szs <62185604+Amethyst-szs@users.noreply.github.com>
2022-03-10 13:35:33 -0800

    Server code cleanup

--- main.js
+++ main.js
@@ -22,9 +22,6 @@ const chalk = require('chalk');
 
 let invalidStage = false;
 
-//Hydration
-let Hydrate = parseInt(fs.readFileSync(`${CurDir}/settings/hydrate.txt`).toString().substring(17), 10);
-
 //Respond to packets from the switch
 server.on('message', (msg, rinfo) => {
     // console.log(msg);
@@ -63,24 +60,26 @@ async function getStreamerAuth(){
     const choices = fs.readdirSync(`${CurDir}/settings/users/`);
     let selection = ``;
 
-    if(choices.length == 0){
-        console.log(`No twitch accounts avaliable, please make some JSONs`);
-        process.exit();
-    } else if (choices.length == 1){
-        console.log(chalk.magenta(`Only one user available, selected automatically`));
-        selection = choices[0];
-    } else {
-        selection = await input.select(choices);
+    switch(choices.length){
+        case 0:
+            console.log(`No twitch accounts avaliable, please make some JSONs`);
+            process.exit();
+        case 1:
+            console.log(chalk.magenta(`Only one user available, selected automatically`));
+            selection = choices[0];
+            break;
+        default:
+            selection = await input.select(choices);
+            break;
     }
 
-    const tokenData = JSON.parse(fs.readFileSync(`${CurDir}/settings/users/${selection}`));
     const authProvider = new AuthAPI.RefreshingAuthProvider(
         {
             clientId,
             clientSecret,
             onRefresh: async newTokenData => await fs.writeFileSync(`${CurDir}/settings/users/${selection}`, JSON.stringify(newTokenData, null, 4))
         },
-        tokenData
+        JSON.parse(fs.readFileSync(`${CurDir}/settings/users/${selection}`))
     );
 
     return authProvider;
@@ -101,25 +100,6 @@ async function TwitchHandler() {
 
     //Create listener that is triggered every channel point redeem
     const listener = await PubSubClient.onRedemption(userId, (message) => {
-        //Dydration
-        switch(message.rewardTitle){
-            case "Hydrate!":
-                Hydrate = parseInt(fs.readFileSync(`${CurDir}/settings/hydrate.txt`).toString().substring(17), 10);
-                Hydrate++;
-                fs.writeFileSync(`${CurDir}/settings/hydrate.txt`, `Hydration Count: ${Hydrate.toString(10)}`);
-                break;
-            case "Dehydrate":
-                Hydrate = parseInt(fs.readFileSync(`${CurDir}/settings/hydrate.txt`).toString().substring(17), 10);
-                Hydrate--;
-                fs.writeFileSync(`${CurDir}/settings/hydrate.txt`, `Hydration Count: ${Hydrate.toString(10)}`);
-                break;
-            case "I Drank Water":
-                Hydrate = parseInt(fs.readFileSync(`${CurDir}/settings/hydrate.txt`).toString().substring(17), 10);
-                Hydrate--;
-                fs.writeFileSync(`${CurDir}/settings/hydrate.txt`, `Hydration Count: ${Hydrate.toString(10)}`);
-                break;
-        }
-
         //Check and make sure a switch has connected already
         if(client.address == undefined){
             log.log(2, `${message.rewardTitle} from ${message.userDisplayName} but no client connected yet!`);
@@ -136,60 +116,8 @@ async function TwitchHandler() {
         log.log(2, `${message.rewardTitle} - ${message.userDisplayName} redeemed!`);
         redeemHistory.updateList(`${message.rewardTitle} < ${message.userDisplayName}`);
 
-        //Switch case through all valid rewards
-        switch(message.rewardTitle){
-            case "Resize": //Packet ID 2 (Resize)
-                outPackets.Resize(server, client);
-                log.log(1, "Resize packet sent");
-                break;
-            case "Position Randomizer": //Packet ID 3 (PosRandomize)
-                outPackets.PosRandomize(server, client);
-                log.log(1, "Position Randomizer packet sent");
-                break;
-            case "See Myself Out": //Event ID 1 (PrevScene)
-                outPackets.Events(server, 1, client);
-                log.log(1, "Event packet sent (See Myself Out)");
-                break;
-            case "Change Gravity": //Event ID 2 (GravFlip)
-                outPackets.Events(server, 2, client);
-                log.log(1, "Event packet sent (Change Gravity)");
-                break;
-            case "Up we go": //Event ID 3 (Fling)
-                outPackets.Events(server, 3, client);
-                log.log(1, "Event packet sent (Up we go)");
-                break;
-            case "Cappy? Nah": //Event ID 4 (Cappy)
-                outPackets.Events(server, 4, client);
-                log.log(1, "Event packet sent (Cappy? Nah)");
-                break;
-            case "Random Kingdom": //Event ID 5 (Random Kingdom)
-                outPackets.Events(server, 5, client);
-                log.log(1, "Event packet sent (Random Kingdom)");
-                break;
-            case "Highway to Death": //Event ID 6 (CoinTick)
-                outPackets.Events(server, 6, client);
-                log.log(1, "Event packet sent (Highway to Death)");
-                break;
-            case "Whispy Winds": //Event ID 7 (Wind)
-                outPackets.Events(server, 7, client);
-                log.log(1, "Event packet sent (Whispy Winds)");
-                break;
-            case "Hot Floor": //Event ID 8
-                outPackets.Events(server, 8, client);
-                log.log(1, "Event packet sent (Hot Floor)");
-                break;
-            case "Stick Flip": //Event ID 9
-                outPackets.Events(server, 9, client);
-                log.log(1, "Event packet sent (Stick Inver)");
-                break;
-            case "Hot Tub Stream": //Event ID 10
-                outPackets.Events(server, 10, client);
-                log.log(1, "Event packet sent (Hot Tub Stream)");
-                break;
-            default: //Generic reward
-                log.log(2, `Generic reward ${message.rewardTitle} redeemed`);
-                break;
-        }
+        //Handle redeem in the out packet handler
+        outPackets.outHandler(message.rewardTitle);
     });
 }
 
--- server_bin/outPackets.js
+++ server_bin/outPackets.js
@@ -1,24 +1,71 @@
 const RedeemSet = require('../settings/redeem_set.json');
 const Rand = require('./advancedRand');
 const bufferTool = require('./bufferTool');
+const log = require('./console');
+
+//Channel point redeem: PrevScene (Server -> Client)
+function Events(server, EventID, client){ //Socket ID 1 - Event ID 1
+    server.send(bufferTool.GenericBuf(1, EventID), client.port, client.address);
+}
+
+//Channel point redeem: Resize (Server -> Client)
+function Resize(server, client){ //Socket ID 2
+    const Amplitude = RedeemSet.Resize.Amplitude;
+    const Scale = [Rand.Ampli(Amplitude, false), Rand.Ampli(Amplitude, false), Rand.Ampli(Amplitude, false)];
+    server.send(bufferTool.Vector3fBuf(2, Scale), client.port, client.address);
+}
+
+//Channel point redeem: PosRandomize (Server -> Client)
+function PosRandomize(server, client){ //Socket ID 3
+    const Amplitude = RedeemSet.PosRandomize.Amplitude;
+    const NewPos = [Rand.Ampli(Amplitude, true), Rand.Ampli(Amplitude, false), Rand.Ampli(Amplitude, true)];
+    server.send(bufferTool.Vector3fBuf(3, NewPos), client.port, client.address);
+} 
 
 module.exports = {
-    //Channel point redeem: PrevScene (Server -> Client)
-    Events: function(server, EventID, client){ //Socket ID 1 - Event ID 1
-        server.send(bufferTool.GenericBuf(1, EventID), client.port, client.address);
-    },
-
-    //Channel point redeem: Resize (Server -> Client)
-    Resize: function(server, client){ //Socket ID 2
-        const Amplitude = RedeemSet.Resize.Amplitude;
-        const Scale = [Rand.Ampli(Amplitude, false), Rand.Ampli(Amplitude, false), Rand.Ampli(Amplitude, false)];
-        server.send(bufferTool.Vector3fBuf(2, Scale), client.port, client.address);
-    },
-
-    //Channel point redeem: PosRandomize (Server -> Client)
-    PosRandomize: function(server, client){ //Socket ID 3
-        const Amplitude = RedeemSet.PosRandomize.Amplitude;
-        const NewPos = [Rand.Ampli(Amplitude, true), Rand.Ampli(Amplitude, false), Rand.Ampli(Amplitude, true)];
-        server.send(bufferTool.Vector3fBuf(3, NewPos), client.port, client.address);
-    } 
-}
+    outHandler: function(redeemName){
+        log.log(1, `Sending redeem packet - ${redeemName}`);
+        //Switch case through all valid rewards
+        switch(redeemName){
+            case "Resize": //Packet ID 2 (Resize)
+                Resize(server, client);
+                break;
+            case "Position Randomizer": //Packet ID 3 (PosRandomize)
+                PosRandomize(server, client);
+                break;
+            case "See Myself Out": //Event ID 1 (PrevScene)
+                Events(server, 1, client);
+                break;
+            case "Change Gravity": //Event ID 2 (GravFlip)
+                Events(server, 2, client);
+                break;
+            case "Up we go": //Event ID 3 (Fling)
+                Events(server, 3, client);
+                break;
+            case "Cappy? Nah": //Event ID 4 (Cappy)
+                Events(server, 4, client);
+                break;
+            case "Random Kingdom": //Event ID 5 (Random Kingdom)
+                Events(server, 5, client);
+                break;
+            case "Highway to Death": //Event ID 6 (CoinTick)
+                Events(server, 6, client);
+                break;
+            case "Whispy Winds": //Event ID 7 (Wind)
+                Events(server, 7, client);
+                break;
+            case "Hot Floor": //Event ID 8
+                Events(server, 8, client);
+                break;
+            case "Stick Flip": //Event ID 9
+                Events(server, 9, client);
+                break;
+            case "Hot Tub Stream": //Event ID 10
+                Events(server, 10, client);
+                break;
+            default: //Generic reward
+                log.log(2, `Generic reward ${message.rewardTitle} redeemed`);
+                break;
+        }
+    }
+};
