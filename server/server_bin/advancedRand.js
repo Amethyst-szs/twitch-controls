cb19dbf7cff5f591fd638290d2e601d7a0e8a48f
Amethyst-szs <Amethyst-UwU@protonmail.com>
2022-03-11 17:09:27 -0800

    Moved server folder, updated logo, fixed bugs

---
+++ server/server_bin/advancedRand.js
@@ -0,0 +1,9 @@
+module.exports = {
+    Ampli: function(Amplitude, canBeNegative){
+        if(canBeNegative){
+            return (Math.random()*(Amplitude*2))-Amplitude;
+        } else {
+            return (Math.random()*Amplitude);
+        }
+    }
+}
--- server_bin/advancedRand.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    Ampli: function(Amplitude, canBeNegative){
-        if(canBeNegative){
-            return (Math.random()*(Amplitude*2))-Amplitude;
-        } else {
-            return (Math.random()*Amplitude);
-        }
-    }
-}
